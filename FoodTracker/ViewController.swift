efa01e0d0f38f8d7ee0c47eca158d881fdfd30f2
Pooja Nadkarni <poojanadkarni@poojas-macbook-pro.local>
2019-01-15 17:39:02 -0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Pooja Nadkarni on 1/15/19.
+//  Copyright © 2019 Pooja Nadkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
